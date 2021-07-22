b563ff8030505917a896a24bbb7ee426ff5e8613
Vivian C <baongocche@gmail.com>
2021-07-22 14:01:41 -0700

    added a bit of frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import {ModalProvider} from "./context/Modal"
+
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 // import * as sessionActions from "./store/session";
 // import * as photoActions from "./store/photo"
+import "./index.css";
 
 const store = configureStore();
 
@@ -23,9 +25,11 @@ if (process.env.NODE_ENV !== "production") {
 function Root() {
   return (
     <Provider store={store}>
+      <ModalProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
