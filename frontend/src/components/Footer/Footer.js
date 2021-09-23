d2c29c83e8423ac7d36990263ffc833df758dbd6
Vivian C <baongocche@gmail.com>
2021-09-23 16:17:44 -0700

    minor footer fixes

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -8,19 +8,17 @@ function Footer() {
         <footer className='footer-container'>
 
                 <div className='top-content'>
-                    <div className='linkedin-github'>
-                        <a className='footer-links' href='https://github.com/itsvivrant/Touristr'>
-                            <i className='fab fa-github'></i>
-                        </a>
-                        <a className='footer-links' href='https://www.linkedin.com/in/vivian-ngoc-che/'>
-                            <i className='fab fa-linkedin'></i>
-                        </a>
-                        <a className='footer-links' href='https://angel.co/u/vivian-ngoc-che'>
-                            <i className="fab fa-angellist"></i>
-                        </a>
-                    </div>
+                    <a className='footer-links' href='https://github.com/itsvivrant/Touristr'>
+                        <i className='fab fa-github'></i>
+                    </a>
+                    <a className='footer-links' href='https://www.linkedin.com/in/vivian-ngoc-che/'>
+                        <i className='fab fa-linkedin'></i>
+                    </a>
+                    <a className='footer-links' href='https://angel.co/u/vivian-ngoc-che'>
+                        <i className="fab fa-angellist"></i>
+                    </a>
                 </div>
-                <div className='footer-user'>Developed by Vivian Ngoc Che</div>
+                <div className='bottom-content'>Developed by Vivian Ngoc Che</div>
         </footer>
     )
 }
